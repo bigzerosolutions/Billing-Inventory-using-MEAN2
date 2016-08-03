f3b52504d7abc4b512fa30295dc2c7739632d146
=vishmealone91 <=vishnuprasad1990@gmail.com>
2016-08-03 21:17:11 +0530

    sessions added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     angular
         .module('app', ['ngRoute', 'ngCookies'])
         .config(config)
+        .run(run);
         
 
     config.$inject = ['$routeProvider', '$locationProvider'];
@@ -49,6 +50,22 @@
             .otherwise({ redirectTo: '/login' });
     }
 
-    
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/home']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
 
 })();
